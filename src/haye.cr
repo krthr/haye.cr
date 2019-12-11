468d94d0a656b737cff7490e7a49cd12ac44af06
krthr <twilson@uninorte.edu.co>
2019-12-11 18:52:10 -0500

    Haye :^)

---
+++ src/haye.cr
@@ -0,0 +1,20 @@
+require "./parsers/**"
+require "./presenters/**"
+require "json"
+
+module Haye
+  VERSION = "0.1.0"
+
+  extend self
+
+  enum PresenterTypes
+    Array
+    Hash
+  end
+
+  def fromPipe(expression : String, type : PresenterTypes = PresenterTypes::Array)
+    type.array? ? Parsers.pipe(expression, Presenters::ArrayPresenter.new).to_array : Parsers.pipe(expression, Presenters::HashPresenter.new).to_hash
+  end
+end
+
+puts Haye.fromPipe("required")
--- src/haye.cr.cr
+++
@@ -1,6 +0,0 @@
-# TODO: Write documentation for `Haye.cr`
-module Haye.cr
-  VERSION = "0.1.0"
-
-  # TODO: Put your code here
-end
